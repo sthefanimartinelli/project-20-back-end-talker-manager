d54991e4f10592793e60e6761d331fd3ea47f9e6
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-04-14 15:38:05 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+const TALKERS_DATA_PATH = './talker.json';
+
+const filePath = path.resolve(__dirname, TALKERS_DATA_PATH);
 
 const app = express();
 app.use(express.json());
@@ -13,4 +18,18 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+// Requisito 1 
+app.get('/talker', async (req, res) => {
+  try {
+    const data = await fs.readFile(filePath);
+    const talkers = JSON.parse(data);
+    if(talkers.length > 0) {
+      return res.status(200).json(talkers);
+    }
+    return res.status(200).json([]);
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`);
+  }
 });
